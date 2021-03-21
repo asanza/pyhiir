14bbc79056a8f245c8310b51d642dcd57ba37e7f
Diego Asanza <f.asanza@gmail.com>
2021-03-21 13:27:17 +0100

    add allpass filter in python
    Signed-off-by: Diego Asanza <f.asanza@gmail.com>

--- hiir.py
+++ hiir.py
@@ -3,6 +3,9 @@
 import ctypes as ct
 import numpy as np
 import os
+from allpass import AllPass, AllPassChain, LowPass
+from scipy import signal
+import matplotlib.pyplot as plt
 
 path = 'build/hiir/libhiir.so'
 
@@ -19,6 +22,9 @@ class hiir:
         self.lib.iir2designer_compute_coefs.restype = ct.c_int
         self.lib.iir2designer_compute_coefs.argtypes = [ct.POINTER(ct.c_double), ct.c_double, ct.c_double]
 
+        self.lib.iir2designer_compute_coefs_spec_order_tbw.argtypes = [ct.POINTER(ct.c_double), ct.c_int, ct.c_double]
+
+
     def compute_nbr_coefs_from_proto(self, attenuation, transition):
         return self.lib.iir2designer_compute_nbr_coefs_from_proto(ct.c_double(attenuation), ct.c_double(transition))
 
@@ -28,7 +34,37 @@ class hiir:
         self.lib.iir2designer_compute_coefs(x.ctypes.data_as(ct.POINTER(ct.c_double)).contents, ct.c_double(attenuation), ct.c_double(transition))
         return x
 
+    def compute_coefs_order_tbw(self, n, transition):
+        x = np.zeros(n, dtype=np.double)
+        self.lib.iir2designer_compute_coefs_spec_order_tbw(x.ctypes.data_as(ct.POINTER(ct.c_double)).contents, ct.c_int(n), ct.c_double(transition))
+        return x
+
 if __name__=='__main__':
     h = hiir()
     #print(h.compute_nbr_coefs_from_proto(40, 0.1))
-    print(h.compute_coefs(80, 0.001))
+    x = h.compute_coefs_order_tbw(10, 0.1)
+    f = LowPass(x)
+    # print(x)    
+    # #print(h.compute_coefs(40, 0.1))
+    # ai = AllPass(2, x[0])
+    # ay = AllPass(2, x[1])
+    # ai1 = AllPass(2, x[2])
+    # ay1 = AllPass(2, x[3])
+    # ai2 = AllPass(2, x[4])
+    # ay2 = AllPass(2, x[5])
+    # ai3 = AllPass(2, x[6])
+    # ay3 = AllPass(2, x[7])
+    # ai4 = AllPass(2, x[8])
+    # ay4 = AllPass(2, x[9])
+
+    # bi = AllPassChain([ai, ai1, ai2, ai3, ai4])
+    # by = AllPassChain([ay, ay1, ay2, ay3, ay4])
+
+    # f = LowPass(bi, by)
+
+    tf = f.get_transfer_function()
+
+    w, h = signal.freqz(tf.b, tf.a)
+
+    plt.plot(w / np.pi, 20 * np.log10(abs(h)), 'b')
+    plt.show()
--- hiir/wrapper.cpp
+++ hiir/wrapper.cpp
@@ -7,6 +7,7 @@ extern "C"
     int iir2designer_compute_nbr_coefs_from_proto(double attenuation, double transition);
     double iir2designer_compute_atten_from_order_tbw(int nbr_coefs, double transition);
     int iir2designer_compute_coefs(double* coef_arr, double attenuation, double transition);
+    void iir2designer_compute_coefs_spec_order_tbw(double coef_arr[], int nbr_coefs, double transition);
 }
 
 int iir2designer_compute_nbr_coefs_from_proto(double attenuation, double transition)
@@ -23,6 +24,11 @@ int iir2designer_compute_coefs(double* coef_arr, double attenuation, double tran
 {
 	return	PolyphaseIir2Designer::compute_coefs(coef_arr, attenuation, transition);
 }
+
+void iir2designer_compute_coefs_spec_order_tbw(double* coef_arr, int nbr_coefs, double transition)
+ {
+     return PolyphaseIir2Designer::compute_coefs_spec_order_tbw(coef_arr, nbr_coefs, transition);
+ }
 
 	// inline void	PolyphaseIir2Designer::compute_coefs_spec_order_tbw(double coef_arr[], int nbr_coefs, double transition)
 	// inline double	PolyphaseIir2Designer::compute_phase_delay(double a, double f_fs)
