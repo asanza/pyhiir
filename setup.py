8dad64c6701ee85e4a74197053d1463c8945ca2d
Diego Asanza <f.asanza@gmail.com>
2021-07-04 11:05:25 +0200

    update
    Signed-off-by: Diego Asanza <f.asanza@gmail.com>

--- setup.py
+++ setup.py
@@ -37,8 +37,7 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(
             os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir, ]#'-DPYTHON_EXECUTABLE=' + sys.executable]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
@@ -93,4 +92,5 @@ setup(
         'scipy >= 1.6.3',
     ],
     zip_safe=False,
+    test_suite='tests',
 )
--- tests/test_hiir.py
+++ tests/test_hiir.py
@@ -5,12 +5,16 @@ from pyhiir.allpass import LowPass
 from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
+import unittest
 
-h = hiir()
-x = h.compute_coefs_order_tbw(2, 0.1)
+class MainTest(unittest.TestCase):
+    def test_create(self):
+        h = hiir()
+        self.assertIsNotNone(h)
+        #x = h.compute_coefs_order_tbw(2, 0.1)
 # f = LowPass(x)
 
-exit(0)
+#exit(0)
 
 # tf = f.get_transfer_function()
 
@@ -19,3 +23,6 @@ exit(0)
 
 # plt.plot(w / np.pi, 20 * np.log10(abs(h)), 'b')
 # plt.show()
+
+if __name__=='__main__':
+    unittest.main()
